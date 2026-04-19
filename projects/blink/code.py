e98401495d14d1e1fd73f029bc7fd4bb3486b9fa
Joshua Lippincott <joshua.m.lippincott@gmail.com>
2026-04-18 20:11:56 -0400

    Update cope.py to follow along with online tutorial examples.

--- projects/blink/code.py
+++ projects/blink/code.py
@@ -7,9 +7,12 @@ led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
 while True:
+    print("Hello Josh")
     led.value = True
     time.sleep(0.2)
     led.value = False
     time.sleep(0.2)
     led.value = True
+    time.sleep(0.2)
+    led.value = False
     time.sleep(1.5)
